03ca96f89d7bf9ded1346e48c99922dbef1d2977
EstefanHu <estefanhu074@gmail.com>
2019-10-12 22:20:06 -0700

    Added image uploader javascipt'

--- Public/js/event.js
+++ Public/js/event.js
@@ -3,7 +3,6 @@
     window.addEventListener("load", init);
 
     function init() {
-        console.log("fuck");
         let post = sessionStorage.getItem('post');
         console.log(post);
     }
