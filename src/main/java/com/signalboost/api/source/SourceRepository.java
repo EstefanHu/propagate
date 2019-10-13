fa1d2f20500f85b5140e6c0270db2a076db8e867
Lukas Strobel <lstrobel@cs.washington.edu>
2019-10-12 18:17:54 -0700

    delete everything

--- src/main/java/com/signalboost/api/source/SourceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.signalboost.api.source;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface SourceRepository extends CrudRepository<Source, Long>{
-	List<Source> findAll();
-}
